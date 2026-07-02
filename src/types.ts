282598bd1ef18bf47c73ed5a6d235b3007a09a4a
Homayoun <homayoun763@gmail.com>
2026-07-02 21:03:13 +0330

    refactor : types and dictionaries

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type Language = "da" | "pa";
+
+export interface Dictionary {
+  ones: string[];
+  teens: string[];
+  tens: string[];
+  hundreds: string[];
+  scales: string[];
+  conjunction: string;
+  currency: string;
+  decimal: string;
+  zero: string;
+}
